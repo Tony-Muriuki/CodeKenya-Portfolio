414377f3062de7f09d555e5fa0de9bade2eb859f
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:16:29 +0300

    Differences Between Variables

---
+++ practice-script.js
@@ -0,0 +1,11 @@
+var featuredProject = "Portfolio Website";
+let skillsCount = 5;
+const siteOwner = "Alex";
+
+// Updating Var and Let Declarations
+featuredProject = "JavaScript Game";
+skillsCount = 8;
+
+// Logging Output
+console.log(featuredProject); // Output: JavaScript Game
+console.log(skillsCount); // Output: 8
