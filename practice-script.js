3611f40b5046e7ddcef33dd9e120d3e053a0a2c4
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:13:31 +0300

    Code Block

--- practice-script.js
+++ practice-script.js
@@ -5,9 +5,15 @@ var projectTitle = "My Portfolio";
 console.log(projectTitle);
 
 // Using Let
-let skillsCount = 5;
+let skillsCount = 4;
 console.log(skillsCount);
 
 // Using Const
 const aboutMe = "Web developer passionate about building projects.";
 console.log(aboutMe);
+
+skillsCount = 5; //Re assigning Variable
+
+if (skillsCount > 3) {
+  console.log("You have lots of skills!");
+}
