9738628b1649988fbe95344bf02f783dce50afda
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:18:37 +0300

    Lesson 07:STrings

--- practice-script.js
+++
@@ -1,4 +0,0 @@
-let projectGallery = "Gallery Section"; // camelCase
-let SkillsList = "Skills Section"; // UpperCamelCase (not recommended for variables)
-let about_me_section = "About Section"; // snake_case
-// let contact-form = "Contact Section";    // kebab-case (not allowed)
