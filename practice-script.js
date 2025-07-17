25a2bd696dc7a3e7d47c90b70e473d6db6d6de20
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:19:25 +0300

    Lesson 07:STrings

---
+++ practice-script.js
@@ -0,0 +1,15 @@
+let projectTitle = "Portfolio Website";
+let aboutSection = "About Me";
+let welcomeMessage = `Welcome to my site!`;
+let projectDescription = "It's a great project!";
+let multiLineWelcome = `
+Welcome to my portfolio!
+Check out my projects below.
+`;
+let name = "Alex";
+let greeting = `Hello, my name is ${name}. Welcome to my portfolio!`;
+let projectsCount = 3;
+let summary = `I have ${projectsCount} featured projects.`;
+
+console.log(greeting);
+console.log(summary);
