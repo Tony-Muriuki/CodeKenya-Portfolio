8e3bd88c1cf5f3b90044edf8daadcf6796b58670
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:11:53 +0300

    Variables and Statements

---
+++ practice-script.js
@@ -0,0 +1,13 @@
+console.log("Hello from practice-script.js!");
+
+// Using Var
+var projectTitle = "My Portfolio";
+console.log(projectTitle);
+
+// Using Let
+let skillsCount = 5;
+console.log(skillsCount);
+
+// Using Const
+const aboutMe = "Web developer passionate about building projects.";
+console.log(aboutMe);
