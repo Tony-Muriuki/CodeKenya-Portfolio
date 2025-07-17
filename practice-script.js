f5e19eb53f632d8615f1a91fced137b59fa17e62
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:23:31 +0300

    Lesson 09:Objects

---
+++ practice-script.js
@@ -0,0 +1,11 @@
+const project = {
+  title: "JavaScript Portfolio",
+  description: "A personal website to showcase my projects and skills.",
+  stars: 42,
+  featured: true,
+};
+
+console.log(project.title);
+console.log(project.stars);
+console.log(project.featured);
+console.log(typeof project);
