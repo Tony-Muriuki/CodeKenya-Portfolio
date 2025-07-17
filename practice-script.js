7f378d7ff1df405041c387255b9c8b9bf2c3d345
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-17 20:27:12 +0300

    Lesson 11:Booleans & Equality

--- practice-script.js
+++ practice-script.js
@@ -1,1 +1,19 @@
 //Booleans and Equality
+let isFeatured = true;
+let hasProfile = false;
+
+const projectStars = 42;
+const isPopular = projectStars > 50;
+console.log(isPopular); // false
+
+let age = 30;
+console.log(age == 30); // true
+console.log(age === 30); // true
+
+console.log(10 == "10"); // true
+console.log(10 === "10"); // false
+
+let featuredCount = 5;
+let featuredCountString = "5";
+console.log(featuredCount == featuredCountString); // true
+console.log(featuredCount === featuredCountString); // false
